47c1fdac4fa4573d2bb5bd45c91c800e9f50b1bf
georgienathan <georgie.nathan@student.ie.edu>
2025-06-27 18:31:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -213,10 +213,12 @@ with tab3:
     # Filter both
     X_clean = X[valid_rows]
     y_clean = y[valid_rows].astype(int)
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+
+    X_train, X_test, y_train, y_test = train_test_split(X_clean, y_clean, random_state=42)
     model = LogisticRegression()
     model.fit(X_train, y_train)
+
 
     # Step 7: Add predictions and metrics
     features = features.loc[X.index]
